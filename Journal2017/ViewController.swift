280ee7e5528ba671aebfb221832e1dccac2607be
Patrick Pahl <patrickpahl@Patricks-MacBook-Pro.local>
2017-04-29 22:13:03 -0600

    InitialCommit

---
+++ Journal2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Journal2017
+//
+//  Created by Patrick Pahl on 4/29/17.
+//  Copyright © 2017 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
