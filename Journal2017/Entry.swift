39c99576c97963de6f3e87bb2d31d603fcf2c590
Patrick Pahl <patrickpahl@Patricks-MacBook-Pro.local>
2017-04-29 22:23:21 -0600

    Commit

---
+++ Journal2017/Entry.swift
@@ -0,0 +1,15 @@
+//
+//  Entry.swift
+//  Journal2017
+//
+//  Created by Patrick Pahl on 4/29/17.
+//  Copyright © 2017 Patrick Pahl. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    
+    
+    
+}
---
+++ Journal2017/EntryTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  EntryTableViewController.swift
+//  Journal2017
+//
+//  Created by Patrick Pahl on 4/29/17.
+//  Copyright © 2017 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+ 
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
